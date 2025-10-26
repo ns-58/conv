ad58ac48129f3a4ff76e1e50637d83ef0d3d4efb
Nikita Shchutskii <nshchutskiy@gmail.com>
2025-10-26 09:58:43 +0300

    partition bench with short analysis

--- Bench/Bench.fs
+++ Bench/Bench.fs
@@ -5,6 +5,8 @@ open BenchmarkDotNet.Running
 open OpenCvSharp
 open System.Runtime.CompilerServices
 open System.Runtime.InteropServices
+open ReadConvWrite
+
 
 let kernel = new Mat([| 3; 3 |], MatType.CV_32FC1, Scalar -1.)
 ~~~ (ind_fl kernel).set_Item 1 1 @@ float32 8
@@ -93,8 +95,103 @@ type PartitionConvComparision() =
         @@ ByRect(Limited @@ ( * ) 10 kernel.Width, UnLimited)
 
 
+let def_cnfg =
+    { source = ([ "/root/conv/pics" ], SearchOption.TopDirectoryOnly)
+      outdir = "./Out"
+      prefix = "Conv"
+      max_readers = Some 1
+      max_writers = None
+      max_conv_performers = Some 4
+      max_queue_size = Some 4
+      conv_mode = ByRow }
+
+
+[<MemoryDiagnoser>]
+type Pipelining() =
+
+    [<Benchmark>]
+    member self.R1C1parW1Qub() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = Some 1
+                max_conv_performers = Some 1
+                max_queue_size = None
+                conv_mode = ByRow }
+            kernel
+
+    [<Benchmark>]
+    member self.R1C1parW1Q1() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = Some 1
+                max_conv_performers = Some 1
+                max_queue_size = Some 1
+                conv_mode = ByRow }
+            kernel
+
+
+    [<Benchmark>]
+    member self.R1C4parW1Q4() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = Some 1
+                max_conv_performers = Some 4
+                max_queue_size = Some 4
+                conv_mode = ByRow }
+            kernel
+
+    [<Benchmark>]
+    member self.R1C4parW1Q8() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = Some 1
+                max_conv_performers = Some 4
+                max_queue_size = Some 8
+                conv_mode = ByRow }
+            kernel
+
+    [<Benchmark>]
+    member self.R1C4parWubQ4() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = None
+                max_conv_performers = Some 4
+                max_queue_size = Some 4
+                conv_mode = ByRow }
+            kernel
+
+    [<Benchmark>]
+    member self.R1CubseqlWubQ16() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = None
+                max_conv_performers = None
+                max_queue_size = Some 16
+                conv_mode = Seql }
+            kernel
+
+    [<Benchmark>]
+    member self.R1C2parWubQ2() =
+        full_cycle_paral_conv
+            { def_cnfg with
+                max_readers = Some 1
+                max_writers = None
+                max_conv_performers = Some 2
+                max_queue_size = Some 4
+                conv_mode = ByRow }
+            kernel
+    
+    [<IterationCleanupAttribute>]
+    member self.Cleanup()  =
+        Directory.Delete (def_cnfg.outdir, recursive = true )
 
 [<EntryPoint>]
 let main _ =
-    BenchmarkRunner.Run typeof<PartitionConvComparision> |> ignore
+    BenchmarkRunner.Run typeof<Pipelining> |> ignore
     0
